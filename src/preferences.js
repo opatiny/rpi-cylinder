8728017324627cf9a43cde43fb8ecb3bc81f7ed0
Océane Patiny <oceane@patiny.com>
2019-03-03 17:54:50 +0100

    fix preferences

--- src/preferences.js
+++ src/preferences.js
@@ -3,7 +3,7 @@
 var cylinderPrototype = 3;
 
 module.exports = {
-    Board:require('raspi-io'),
+    Board:require('raspi-io').RaspiIO,
     servoPins:[0, 1, 2],
     cylinderPrototype: require('./prefs/cylinderPrototype' + cylinderPrototype)
 };
